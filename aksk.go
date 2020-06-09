3927c399f3a79f7fe85538c184bfdd48fbc15146
wuqingtao <wuqingtao@antlinker.com>
2020-06-09 17:34:12 +0800

    update

---
+++ aksk.go
@@ -0,0 +1,28 @@
+// Package aksk 实现http的中间件, 用于认证客户端请求和校验请求内容
+package aksk
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/qingtao/aksk/core"
+	akskgin "github.com/qingtao/aksk/middleware/gin"
+	akskhttp "github.com/qingtao/aksk/middleware/http"
+)
+
+// HTTPMiddleware http中间件
+type HTTPMiddleware = akskhttp.Middleware
+
+// HTTPErrorHandler http中间件的错误处理函数
+type HTTPErrorHandler = akskhttp.ErrorHandler
+
+// DefaultHTTPMiddleware 新的http中间件
+func DefaultHTTPMiddleware(fn core.KeyFunc, errHandler HTTPErrorHandler) *HTTPMiddleware {
+	return akskhttp.New(akskhttp.Config{Key: fn, ErrorHandler: errHandler})
+}
+
+// GINErrorHandler gin中间件的错误处理函数
+type GINErrorHandler = akskgin.ErrorHandler
+
+// DefaultGINMiddleware 新的gin中间件
+func DefaultGINMiddleware(fn core.KeyFunc, errHandler GINErrorHandler) gin.HandlerFunc {
+	return akskgin.New(akskgin.Config{Key: fn, ErrorHandler: errHandler})
+}
